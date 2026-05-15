692ddf9a3f10d404a01a01ed31a64361026276dd
Danila Poyarkov <dev@dannote.net>
2026-05-15 13:13:24 +0300

    refactor(toolbar): reuse test id helpers

--- tests/helpers/test-ids.ts
+++ tests/helpers/test-ids.ts
@@ -1,11 +1,6 @@
-export function toolbarFlyoutTestId(tool: string, mobile = false) {
-  return `${mobile ? 'mobile-' : ''}toolbar-flyout-${tool.toLowerCase()}`
-}
-
-export function toolbarFlyoutItemTestId(tool: string, mobile = false) {
-  return `${mobile ? 'mobile-' : ''}toolbar-flyout-item-${tool.toLowerCase()}`
-}
-
-export function variablesAddTestId(type: string) {
-  return `variables-add-${type.toLowerCase()}`
-}
+export {
+  toolbarFlyoutItemTestId,
+  toolbarFlyoutTestId,
+  toolbarToolTestId,
+  variablesAddTestId
+} from '#vue/testing/test-id'
